714e91d3d92a83446c90fbebb524290ae69c2b9a
Andrew Smith <andrew@andrew.codes>
2020-12-04 11:50:57 -0500

    make it easier to run with machine passwords

--- k8s_reset.sh
+++
@@ -1,5 +0,0 @@
-source .secrets/install_k8s.vars.sh
-ANSIBLE_CONFIG=ansible/ansible.cfg
-
-ansible-playbook ansible/k8s/reset_k8s.yml -i ansible/hosts --extra-vars "ansible_become_pass='$MACHINE_PASSWORD'"
-rm -f ansible/k8s/playbooks/kubernetes_join_command
