7853a243f974107e6174c33ac289031df1f1e24f
Andrew Smith <andrew@andrew.codes>
2020-12-11 14:48:25 -0500

    first stab

---
+++ scripts/powershell/set_windows_ssh_keys_from_github.ps1
@@ -0,0 +1,20 @@
+# DO the following in powerhsell if not already done:
+# Set-ExecutionPolicy RemoteSigned
+
+$GitHubUserName=""
+(Invoke-WebRequest "https://github.com/$GitHubUserName.keys").Content | Out-File -FilePath "$env:userprofile\.ssh\authorized_keys"
+
+# NOTE: edit the path in this command if needed
+$sshFiles=Get-ChildItem -Path "$env:userprofile\.ssh" -Force
+
+$sshFiles | % {
+  $key = $_
+  & icacls $key /c /t /inheritance:d
+  & icacls $key /c /t /grant  "${echo $env:username}":F
+  & icacls $key /c /t /remove Administrator "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+}
+
+# Verify:
+$sshFiles | % {
+  icacls $_
+}
