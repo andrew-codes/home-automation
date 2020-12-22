b3c9c000abc96bfc9f89e21c5344cb94f0ef57eb
Andrew Smith <andrew@andrew.codes>
2020-12-22 11:03:16 -0500

    test output machine password

--- apps/playnite-game-data-exporter/scripts/bin/deploy.sh
+++ apps/playnite-game-data-exporter/scripts/bin/deploy.sh
@@ -5,4 +5,5 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 ANSIBLE_CONFIG=../../ansible/ansible.cfg
 
 wakeonlan $GAMING_ROOM_GAMING_PC_MAC
+echo "$MACHINE_PASSWORD"
 ansible-playbook "scripts/bin/deploy.yml" -i ../../ansible/hosts.yml --extra-vars "ansible_become_pass='$MACHINE_PASSWORD' ansible_password='$MACHINE_PASSWORD'"
