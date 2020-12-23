1da1be2d6fc9ec3244c3b5f924cfa907521d747c
Andrew Smith <andrew@andrew.codes>
2020-12-23 11:00:42 -0500

    wake gaming pc when redis init container runs

--- apps/game-cache-primer/src/index.ts
+++ apps/game-cache-primer/src/index.ts
@@ -1,12 +1,20 @@
 import createDebugger from "debug"
 import { connectAsync } from "async-mqtt"
+import wol from "wakeonlan"
 
 const debug = createDebugger("@ha/game-cache-primer-app/index")
 
-const { MQTT_HOST, MQTT_PASSWORD, MQTT_PORT, MQTT_USERNAME } = process.env
+const {
+  GAMING_ROOM_GAMING_PC_MAC,
+  MQTT_HOST,
+  MQTT_PASSWORD,
+  MQTT_PORT,
+  MQTT_USERNAME,
+} = process.env
 
 const run = async () => {
   debug("Starting app.")
+  await wol(GAMING_ROOM_GAMING_PC_MAC)
   const mqtt = await connectAsync(`tcp://${MQTT_HOST}`, {
     password: MQTT_PASSWORD,
     port: parseInt(MQTT_PORT || "1883", 10),
--- k8s/game-cache/scripts/bin/seal.sh
+++ k8s/game-cache/scripts/bin/seal.sh
@@ -11,3 +11,10 @@ kubectl create secret generic redis-secrets --dry-run=client --namespace="home-a
 kubeseal --namespace "home-automation" <".secrets/redis-secrets.json" >"secrets/redis-secrets.json"
 rm -rf .secrets/redis-password
 rm -rf .secrets/redis-secrets.json
+
+# Gaming PC
+echo -n "$GAMING_ROOM_GAMING_PC_MAC" >.secrets/gaming-pc-mac
+kubectl create secret generic gaming-pc-secrets --dry-run=client --namespace="home-automation" --from-file=mac=".secrets/gaming-pc-mac" -o json >".secrets/gaming-pc-secrets.json"
+kubeseal --namespace "home-automation" <".secrets/gaming-pc-secrets.json" >"secrets/gaming-pc-secrets.json"
+rm -rf .secrets/gaming-pc-mac
+rm -rf .secrets/gaming-pc-secrets.json
