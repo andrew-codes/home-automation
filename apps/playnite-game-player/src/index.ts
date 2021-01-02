b0d78b1f25612f73def28cacd8806da0174dd824
Andrew Smith <andrew@andrew.codes>
2021-01-02 17:01:57 -0500

    there may not be a `.debug`; changing to info

--- apps/playnite-game-player/src/index.ts
+++ apps/playnite-game-player/src/index.ts
@@ -17,43 +17,47 @@ debug.warn("Watch out!")
 debug.error("Something went wrong.")
 
 async function run() {
-  debug.debug("Connecting MQTT client")
-  const mqtt = await connectAsync(`tcp://${MQTT_HOST}`, {
-    password: MQTT_PASSWORD,
-    port: parseInt(MQTT_PORT || "1883", 10),
-    username: MQTT_USERNAME,
-  })
+  debug.info("Connecting MQTT client")
+  try {
+    const mqtt = await connectAsync(`tcp://${MQTT_HOST}`, {
+      password: MQTT_PASSWORD,
+      port: parseInt(MQTT_PORT || "1883", 10),
+      username: MQTT_USERNAME,
+    })
 
-  await mqtt.subscribe("/playnite/game/play/pc")
-  await mqtt.subscribe("/playnite/game/stop/pc")
+    await mqtt.subscribe("/playnite/game/play/pc")
+    await mqtt.subscribe("/playnite/game/stop/pc")
 
-  mqtt.on("message", async (topic, message) => {
-    if (topic === "/playnite/game/play") {
-      const { id, platform } = JSON.parse(message.toString())
-      if (platform !== "pc") {
-        return
-      }
-      debug.debug(`Playing PC game ${id}`)
-      exec(`${PLAYNITE_EXEC} --start ${id}`, (code, stdout, stderr) => {
-        debug.info(`Exit code ${code}`)
-      })
-    }
-
-    if (topic === "/playnite/game/stop/pc") {
-      const { platform } = JSON.parse(message.toString())
-      if (platform !== "pc") {
-        return
-      }
-
-      list((services) => {
-        const pid = services.find((svc) => svc.SessionName === "Playnite")
-        debug.debug(`PID: ${pid}`)
-        kill(pid, () => {
-          debug.info("Playnite stopped")
+    mqtt.on("message", async (topic, message) => {
+      if (topic === "/playnite/game/play") {
+        const { id, platform } = JSON.parse(message.toString())
+        if (platform !== "pc") {
+          return
+        }
+        debug.info(`Playing PC game ${id}`)
+        exec(`${PLAYNITE_EXEC} --start ${id}`, (code, stdout, stderr) => {
+          debug.info(`Exit code ${code}`)
         })
-      }, true)
-    }
-  })
+      }
+
+      if (topic === "/playnite/game/stop/pc") {
+        const { platform } = JSON.parse(message.toString())
+        if (platform !== "pc") {
+          return
+        }
+
+        list((services) => {
+          const pid = services.find((svc) => svc.SessionName === "Playnite")
+          debug.info(`PID: ${pid}`)
+          kill(pid, () => {
+            debug.info("Playnite stopped")
+          })
+        }, true)
+      }
+    })
+  } catch (error) {
+    debug.info(error)
+  }
 }
 
 run()
