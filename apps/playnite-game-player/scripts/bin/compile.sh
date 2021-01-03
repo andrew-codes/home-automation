90000508c5e0c33022f9e2be3a6867eb6cf376d9
Andrew Smith <andrew@andrew.codes>
2021-01-03 11:44:46 -0500

    use github secrets names

--- apps/playnite-game-player/scripts/bin/compile.sh
+++ apps/playnite-game-player/scripts/bin/compile.sh
@@ -3,8 +3,8 @@
 mkdir -p dist && cp -r src/* dist
 cat <<EOL >>dist/secrets.js
 module.exports = {
-    MQTT_HOST : "$CLUSTER_IP",
-    MQTT_PORT : $EXTERNAL_MQTT_PORT,
+    MQTT_HOST : "$MQTT_HOST",
+    MQTT_PORT : $MQTT_PORT,
     MQTT_USERNAME : "$MQTT_USERNAME",
     MQTT_PASSWORD : "$MQTT_PASSWORD",
 }
