4fc3bb8f5ed1112a3f13bc31ee71192225b480da
Andrew Smith <andrew@andrew.codes>
2021-01-25 08:05:19 -0500

    bump controller version

--- k8s/github-action-runners/scripts/bin/deploy.sh
+++ k8s/github-action-runners/scripts/bin/deploy.sh
@@ -2,5 +2,5 @@
 
 kubectl apply -f namespace.yml
 kubectl apply -f secrets
-kubectl apply -f https://github.com/summerwind/actions-runner-controller/releases/download/v0.15.0/actions-runner-controller.yaml
+kubectl apply -f https://github.com/summerwind/actions-runner-controller/releases/download/v0.16.1/actions-runner-controller.yaml
 kubectl apply -f runners.yml
