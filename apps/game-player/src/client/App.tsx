87dad7fca7cd440b0cd2963d7b31c0753ea7a906
Andrew Smith <andrew@andrew.codes>
2021-02-06 18:16:21 -0500

    wip: full width/height
    need to remove the margins on the body

--- apps/game-player/src/client/App.tsx
+++ apps/game-player/src/client/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import AutoSizer from "react-virtualized-auto-sizer"
 import { FixedSizeGrid as Grid } from "react-window"
-import { AppBar, Drawer, Toolbar } from "@material-ui/core"
+import { AppBar, Container, Drawer, Toolbar } from "@material-ui/core"
 
 const Cell = ({ columnIndex, rowIndex, style }) => (
   <div style={style}>
@@ -19,20 +19,24 @@ const App = () => {
       <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
         <h1>Filters</h1>
       </Drawer>
-      <AutoSizer>
-        {(height, width) => (
-          <Grid
-            columnCount={1000}
-            columnWidth={100}
-            height={500}
-            rowCount={1000}
-            rowHeight={35}
-            width={1000}
-          >
-            {Cell}
-          </Grid>
-        )}
-      </AutoSizer>
+      <div
+        style={{ height: "calc(100vh - 64px)", width: "calc(100vw - 35px)" }}
+      >
+        <AutoSizer>
+          {({ height, width }) => (
+            <Grid
+              columnCount={1000}
+              columnWidth={100}
+              height={height}
+              rowCount={1000}
+              rowHeight={35}
+              width={width}
+            >
+              {Cell}
+            </Grid>
+          )}
+        </AutoSizer>
+      </div>
     </>
   )
 }
