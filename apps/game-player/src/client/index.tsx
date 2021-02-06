cc173608d4b11aae46c1a27a0e631990f65e426c
Andrew Smith <andrew@andrew.codes>
2021-02-06 15:37:06 -0500

    enable apollo client in components

--- apps/game-player/src/client/index.tsx
+++ apps/game-player/src/client/index.tsx
@@ -1,6 +1,17 @@
-import * as React from 'react'
+import * as React from "react"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import { render } from "react-dom"
 import { App } from "./App"
 
+const client = new ApolloClient({
+  uri: "/api",
+  cache: new InMemoryCache(),
+})
+
 const appEl = document.getElementById("app")
-render(<App />, appEl)
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  appEl
+)
