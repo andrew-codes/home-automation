7a73a852ffb4a8854808caa0c7a5dbbdd5b8dddd
Andrew Smith <andrew@andrew.codes>
2021-02-07 16:52:29 -0500

    provide playnite id and platform directly to graphql qpi

--- apps/graphql-api/src/schema/mutations.ts
+++ apps/graphql-api/src/schema/mutations.ts
@@ -98,11 +98,12 @@ export const PlayGameInGameRoomMutation = mutationField("playGameInGameRoom", {
   type: GameGraphType,
   args: {
     id: stringArg(),
+    platformName: stringArg(),
   },
   async resolve(root, args, ctx) {
     try {
       debug("Args", args)
-      if (!args.id) {
+      if (!args.id || !args.platformName) {
         throw new Error("Invalid argument")
       }
       const currentGameResults = (await ctx.query({
@@ -115,27 +116,7 @@ export const PlayGameInGameRoomMutation = mutationField("playGameInGameRoom", {
         throw new Error(`A game is already running: ${currentGame.name}`)
       }
 
-      const gameToPlay = (await ctx.query({
-        from: "game",
-        filters: [equality<DomainGame>("playniteId", args.id)],
-      })) as GameEntity
-
-      if (isEmpty(gameToPlay)) {
-        throw new Error(`No matching game found for ID: ${args.id}`)
-      }
-
-      const gamePlatform = (await ctx.query({
-        from: "game_platform",
-        filters: [equality<DomainGamePlatform>("id", gameToPlay.platformId)],
-      })) as GamePlatform
-
-      if (isEmpty(gamePlatform)) {
-        throw new Error(
-          `Could not find game platform: ${gameToPlay.platformId}`
-        )
-      }
-
-      const normalizedPlatform = normalizePlatform(gamePlatform)
+      const normalizedPlatform = normalizePlatform(args.platformName)
       debug(
         "Turning on media player",
         `media_player.gaming_room_universal_${normalizedPlatform}`
@@ -150,28 +131,26 @@ export const PlayGameInGameRoomMutation = mutationField("playGameInGameRoom", {
       debug(turnOnMediaPlayerRepsonse)
 
       if (normalizedPlatform === "gaming_pc") {
-        debug("Publishing play pc game", gameToPlay.playniteId)
+        debug("Publishing play pc game", args.id)
         await ctx.mqtt.publish(
           `/playnite/game/play`,
-          JSON.stringify({ id: gameToPlay.playniteId, platform: "pc" })
+          JSON.stringify({ id: args.id, platform: "pc" })
         )
       } else if (normalizedPlatform === "playstation_4_pro") {
         await ctx.mqtt.publish(
           `/playnite/game/play`,
-          JSON.stringify({ id: gameToPlay.playniteId, platform: "ps4" })
+          JSON.stringify({ id: args.id, platform: "ps4" })
         )
-        debug("Setting source for PS4 media player", gameToPlay.name)
         const setSourceResponse = await ctx.ha.services.call(
           "select_source",
           "media_player",
           {
             entity_id: `media_player.gaming_room_universal_${normalizedPlatform}`,
-            source: gameToPlay.name,
+            source: args.id,
           }
         )
         debug(setSourceResponse)
       }
-      return gameToPlay
     } catch (error) {
       debug(error)
     }
