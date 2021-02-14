01f72734a3535d33504c1cdcabb267f9a4a12478
Andrew Smith <andrew@andrew.codes>
2021-02-14 15:52:04 -0500

    wallaby and jest should report the same coverage data

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,12 @@
-module.exports = require("@ha/jest-config")
+const { defaults } = require("jest-config")
+
+module.exports = {
+  ...defaults,
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  collectCoverageFrom: [
+    "**/src/**/*.{js,ts}",
+    "!**/__tests__/*.*",
+    "!node_modules/**",
+    "!**/node_modules/**",
+  ],
+}
