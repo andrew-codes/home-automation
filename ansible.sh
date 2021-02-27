d923b4c25fa7ddd1aa858d260258181fcff5a706
Andrew Smith <andrew@andrew.codes>
2021-02-27 16:50:47 -0500

    fix ansible errors
    Removing secrets for k8s happens before ansible has completed
    Include vars for ansible playbook using them
    Sytnax error with quotation

--- ansible.sh
+++ ansible.sh
@@ -6,5 +6,3 @@ source secrets.sh
 ANSIBLE_CONFIG=./ansible/ansible.cfg
 
 ansible-playbook "$1" -i ansible/hosts.yml --extra-vars "ansible_become_pass='$MACHINE_PASSWORD'"
-
-rm -rf ansible/k8s/.secrets
