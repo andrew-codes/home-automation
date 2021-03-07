d8596022fe3dc088c36bb2f94eafc433deedf5b6
Andrew Smith <andrew@andrew.codes>
2021-03-07 11:59:09 -0500

    requires host network

--- k8s/ps5-wake/scripts/bin/seal.sh
+++ k8s/ps5-wake/scripts/bin/seal.sh
@@ -6,6 +6,11 @@ mkdir -p secrets
 mkdir -p .secrets
 
 echo -n "$PS5_USER_CREDENTIALS" >.secrets/ps5-user-credentials
-kubectl create secret generic ps5-wake-secrets --dry-run=client --namespace="home-automation" --from-file=ps5-user-credentials=".secrets/ps5-user-credentials" -o json >".secrets/ps5-secrets.json"
+echo -n "$CLUSTER_IP" >.secrets/mqtt-host
+echo -n "$EXTERNAL_MQTT_PORT" >.secrets/mqtt-port
+kubectl create secret generic ps5-wake-secrets --dry-run=client --namespace="home-automation" --from-file=ps5-user-credentials=".secrets/ps5-user-credentials" --from-file=mqtt-host=".secrets/mqtt-host" --from-file=mqtt-port=".secrets/mqtt-port" -o json >".secrets/ps5-secrets.json"
 kubeseal --namespace "home-automation" <".secrets/ps5-secrets.json" >"secrets/ps5-secrets.json"
 rm -rf .secrets/ps5-secrets.json
+rm -rf .secrets/ps5-user-credentials
+rm -rf .secrets/mqtt-host
+rm -rf .secrets/mqtt-port
