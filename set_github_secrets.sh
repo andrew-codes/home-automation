25bedc231f3c8bcee7a757f0aede238ad3c998ae
Andrew Smith <andrew@andrew.codes>
2021-03-09 07:58:56 -0500

    use valid token name

--- set_github_secrets.sh
+++ set_github_secrets.sh
@@ -3,7 +3,7 @@
 source secrets.sh
 
 yarn seal-github-secret andrew-codes home-automation DOCKER_REGISTRY_DOMAIN "$DOCKER_REGISTRY_DOMAIN"
-yarn seal-github-secret andrew-codes home-automation GITHUB_TOKEN "$GITHUB_ACTION_JEST_REPORTER_TOKEN"
+yarn seal-github-secret andrew-codes home-automation JEST_REPORTER_TOKEN "$GITHUB_ACTION_JEST_REPORTER_TOKEN"
 yarn seal-github-secret andrew-codes home-automation DOCKER_REGISTRY_USERNAME "$DOCKER_REGISTRY_USERNAME"
 yarn seal-github-secret andrew-codes home-automation DOCKER_REGISTRY_PASSWORD "$DOCKER_REGISTRY_PASSWORD"
 yarn seal-github-secret andrew-codes home-automation GAMING_ROOM_GAMING_PC_MAC "$GAMING_ROOM_GAMING_PC_MAC"
