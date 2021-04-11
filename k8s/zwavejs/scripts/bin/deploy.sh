8d15a0e77dcb36eddcd2f9d5982c457199fc47f0
Andrew Smith <andrew@andrew.codes>
2021-04-11 10:01:15 -0400

    attempt to get connected to HA

--- k8s/zwavejs/scripts/bin/deploy.sh
+++ k8s/zwavejs/scripts/bin/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 source ../../external-port-vars.sh
-yq eval '.spec.ports[0].nodePort=env(EXTERNAL_ZWAVE_JS_MQTT_PORT)' -i service.yml
+yq eval '.spec.ports[0].nodePort=env(EXTERNAL_ZWAVE_JS_PORT)' -i service.yml
+yq eval '.spec.ports[1].nodePort=env(EXTERNAL_ZWAVE_JS_WS_PORT)' -i service.yml
 
 kubectl apply -f namespace.yml
 kubectl apply -f deployment.yml
