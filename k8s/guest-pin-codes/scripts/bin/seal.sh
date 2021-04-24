5526e1bee190326585d72fa97fccd39e1718bd60
Andrew Smith <andrew@andrew.codes>
2021-04-24 15:54:47 -0400

    #333

--- k8s/guest-pin-codes/scripts/bin/seal.sh
+++ k8s/guest-pin-codes/scripts/bin/seal.sh
@@ -17,7 +17,9 @@ rm -rf .secrets/service_account
 rm -rf .secrets/google-guest-calendar-secrets.json
 
 echo -n "$GUEST_LOCK_ENTITY_PREFIXES" >.secrets/guest_lock_entity_prefixes
-kubectl create secret generic guest-lock-entity-secrets --dry-run=client --namespace="home-automation" --from-file=guest_lock_entity_prefixes=".secrets/guest_lock_entity_prefixes" -o json >".secrets/guest-lock-entity-prefixes.json"
+echo -n "$GUEST_LOCK_CODE_EXCLUSIONS" >.secrets/guest_lock_code_exclusions
+kubectl create secret generic guest-lock-entity-secrets --dry-run=client --namespace="home-automation" --from-file=guest_lock_entity_prefixes=".secrets/guest_lock_entity_prefixes" --from-file=guest_lock_code_exclusions=".secrets/guest_lock_code_exclusions" -o json >".secrets/guest-lock-entity-prefixes.json"
 kubeseal --namespace "home-automation" <".secrets/guest-lock-entity-prefixes.json" >"secrets/guest-lock-entity-secrets.json"
 rm -rf .secrets/guest_lock_entity_prefixes
+rm -rf .secrets/guest_lock_code_exclusions
 rm -rf .secrets/guest-lock-entity-prefixes.json
