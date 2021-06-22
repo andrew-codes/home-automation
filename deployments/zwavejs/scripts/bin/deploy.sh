8c4cdd433ae241d52a9433aa0ca3b78c2baf33e1
Andrew Smith <andrew@andrew.codes>
2021-06-22 12:47:33 +0000

    zwave conversion

--- deployments/zwavejs/scripts/bin/deploy.sh
+++ deployments/zwavejs/scripts/bin/deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-source ../../external-port-vars.sh
+pushd .
+cd ../../
+set -o allexport
+source .external-ports.env
+set +o allexport
+popd
+
 yq eval '.spec.ports[0].nodePort=env(EXTERNAL_ZWAVE_JS_PORT)' -i service.yml
 yq eval '.spec.ports[1].nodePort=env(EXTERNAL_ZWAVE_JS_WS_PORT)' -i service.yml
 
-kubectl apply -f namespace.yml
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
