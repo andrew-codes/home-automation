885dbd69d99d180c0f157ed200030ce0da7bc4c5
Andrew Smith <andrew@andrew.codes>
2021-06-24 14:38:40 +0000

    fixes for github action runner image

--- apps/github-action-runner-amd64/scripts/bin/image-push.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -v $PWD:/workspace gcr.io/kaniko-project/executor:latest --destination "docker-registry:5000/github-action-runner:latest" --dockerfile /workspace/Dockerfile --context dir:///workspace/ --insecure
