6881c6a0319f43b5ea9d7051d857f5b275d85161
Andrew Smith <andrew@andrew.codes>
2021-06-25 13:03:23 +0000

    fix typo for shell script

--- apps/playnite-game-player/scripts/bin/compile.sh
+++ apps/playnite-game-player/scripts/bin/compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p dist && cp -r src/* dist
-cat <<EOL >>dist/secrets.js
+cat <<EOL >dist/secrets.js
 module.exports = {
     MQTT_HOST : "$MQTT_HOST",
     MQTT_PORT : $MQTT_PORT,
