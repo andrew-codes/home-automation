2c82c77079e5253d4ad85c2de433aab261d5f6c1
Andrew Smith <andrew@andrew.codes>
2021-07-03 13:53:45 -0400

    attempt to run ssh creds in remote dev container

--- .devcontainer/wrapdocker.sh
+++ .devcontainer/wrapdocker.sh
@@ -84,23 +84,18 @@ rm -rf /var/run/docker.pid
 
 # If we were given a PORT environment variable, start as a simple daemon;
 # otherwise, spawn a shell as well
-if [ "$PORT" ]; then
-    exec dockerd -H 0.0.0.0:$PORT -H unix:///var/run/docker.sock \
-        $DOCKER_DAEMON_ARGS
+if [ "$LOG" == "file" ]; then
+    dockerd $DOCKER_DAEMON_ARGS &>/var/log/docker.log &
 else
-    if [ "$LOG" == "file" ]; then
-        dockerd $DOCKER_DAEMON_ARGS &>/var/log/docker.log &
-    else
-        dockerd $DOCKER_DAEMON_ARGS &
-    fi
-    ((timeout = 60 + SECONDS))
-    until docker info >/dev/null 2>&1; do
-        if ((SECONDS >= timeout)); then
-            echo 'Timed out trying to connect to internal docker host.' >&2
-            break
-        fi
-        sleep 1
-    done
-    [[ $1 ]] && exec "$@"
-    exec bash --login
+    dockerd $DOCKER_DAEMON_ARGS &
 fi
+((timeout = 60 + SECONDS))
+until docker info >/dev/null 2>&1; do
+    if ((SECONDS >= timeout)); then
+        echo 'Timed out trying to connect to internal docker host.' >&2
+        break
+    fi
+    sleep 1
+done
+[[ $1 ]] && exec "$@"
+exec bash --login
