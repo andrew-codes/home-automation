b655cacfb9710656633719fcba91a2a7b1b24d06
Andrew Smith <andrew@andrew.codes>
2021-07-05 16:07:51 +0000

    rm extra home-assitant directory

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -10,6 +10,7 @@ cp -rf ${ROOT}/vault/secrets/secrets.yaml /home-assistant
 if [ ! -z "$TELEPRESENCE_ROOT" ]; then
     sudo -u root rsync --recursive -I --exclude="*.db*" --exclude ".git" ${ROOT}/home-assistant/* /home-assistant
     sudo -u root rsync --recursive -I --exclude="*.db*" /home-assistant-src/* /home-assistant
+    rm -rf /home-assistant/home-assistant
 fi
 
 hass --config /home-assistant
