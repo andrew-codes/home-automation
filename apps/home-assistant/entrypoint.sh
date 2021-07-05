5b5e0e4a7eb4817f07da0eb3ec55456099db3091
Andrew Smith <andrew@andrew.codes>
2021-07-05 13:54:26 +0000

    ha config controlled by git
    with HACS installed

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -5,15 +5,8 @@ export ROOT="${TELEPRESENCE_ROOT:-}"
 source ${ROOT}/vault/secrets/env.sh
 set +e
 source ${ROOT}/root/set_ssh_keys.sh
-cp -rf /home-assistant-src/* /home-assistant
 rm -rf /home-assistant/secrets.yaml
 cp -rf ${ROOT}/vault/secrets/secrets.yaml /home-assistant
-
-if [ ! -z "TELEPRESENCE_ROOT" ]; then
-    sudo cp -rf ${ROOT}/home-assistant /home-assistant-prod
-    sudo cp -rf /home-assistant-prod/* /home-assistant
-    cp -rf /home-assistant-src/* /home-assistant
-fi
-
 set -e
+
 hass --config /home-assistant
