5cc6742e4583bcc7e5bc1030451dd74f34901fc6
Andrew Smith <andrew@andrew.codes>
2021-07-05 15:10:18 +0000

    properly enable root permission access to remote volumes

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -3,16 +3,13 @@
 export ROOT="${TELEPRESENCE_ROOT:-}"
 
 source ${ROOT}/vault/secrets/env.sh
-set +e
 source ${ROOT}/root/set_ssh_keys.sh
-set -e
 rm -rf /home-assistant/secrets.yaml
 cp -rf ${ROOT}/vault/secrets/secrets.yaml /home-assistant
 
-if [ ! -z "TELEPRESENCE_ROOT" ]; then
-    sudo -u root source ${ROOT}/root/set_ssh_keys.sh
-    sudo -u root cp -rf ${ROOT}/home-assistant/* /home-assistant
-    cp -rf /home-assistant-src/* /home-assistant
+if [ ! -z "$TELEPRESENCE_ROOT" ]; then
+    sudo -u root rsync --recursive -I --exclude="*.db*" --exclude ".git" ${ROOT}/home-assistant/* /home-assistant
+    sudo -u root rsync --recursive -I --exclude="*.db*" /home-assistant-src/* /home-assistant
 fi
 
 hass --config /home-assistant
--- apps/home-assistant/scripts/bin/start.sh
+++ apps/home-assistant/scripts/bin/start.sh
@@ -3,4 +3,4 @@
 echo "Will Start on Port 8123"
 
 docker pull docker-registry:5000/home-assistant:latest
-telepresence --swap-deployment "home-assistant" --docker-run --rm -t -v "$PWD/src:/home-assistant-src" -p "8123:8123" "docker-registry:5000/home-assistant:latest"
+telepresence --swap-deployment "home-assistant" --expose 9999:81 --docker-run --rm -t -v "$PWD/src:/home-assistant-src" -p "8123:8123" "docker-registry:5000/home-assistant:latest"
