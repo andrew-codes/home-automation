f362507dfd47e7c8c6220e20f05b6f1047929f30
Andrew Smith <andrew@andrew.codes>
2021-07-09 21:10:04 +0000

    clean up start locally script

--- scripts/bin/start-locally.sh
+++ scripts/bin/start-locally.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-source ../../secrets.sh
-
 function start-locally() {
     : ${1?"Usage: $0; first argument must the k8s namespace"} ${2?"Usage: $0; second argument is the deployment name"} ${3?"Usage: $0; third argument is dev docker image"}
 
@@ -9,8 +7,8 @@ function start-locally() {
 
     if [ "$NODE_APP_IMAGE_COUNT_BY_REFERENCE" -ne "2" ]; then
         echo "No image found for $2:latest, building developer image first for app package: @ha/$2-app"
-        yarn image/local
+        $(cd ../../ && yarn image/local)
     fi
 
-    telepresence --namespace "$1" --swap-deployment "$2" --docker-run --rm -t -v "$PWD/../../:/app" -p "$4:80" "$3" yarn lerna run start/dev --scope "@ha/$2-app" --stream <<<$DEV_MACHINE_PASSWORD
+    telepresence --namespace "$1" --swap-deployment "$2" --docker-run --rm -t -v "$PWD:/app" -p "$4:80" "$3" yarn start/dev --stream
 }
