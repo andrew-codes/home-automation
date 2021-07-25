f2442f2a301a8620f710c2ffa67cd4a42a372bf2
Andrew Smith <andrew@andrew.codes>
2021-07-25 20:08:31 +0000

    users can report issues to Jira
    HOME-2

--- deployments/home-assistant/scripts/bin/initialize-secrets.sh
+++ deployments/home-assistant/scripts/bin/initialize-secrets.sh
@@ -31,6 +31,7 @@ vault kv put kv/home-assistant \
     HOME_AUTOMATION_PRIVATE_SSH_KEY= "" \
     HOME_AUTOMATION_PUBLIC_SSH_KEY= "" \
     INLETS_IP="" \
+    JIRA_API_TOKEN="" \
     LATITUDE="" \
     LONGITUDE="" \
     MS_TEAMS_STATUS_ACTIVE_WEBHOOK_ID="" \
