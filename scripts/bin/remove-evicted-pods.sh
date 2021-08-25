9a87fd5e65dd44f45ccf8ca58ede3914bddb7a14
Andrew Smith <andrew@andrew.codes>
2021-08-25 12:50:00 +0000

    correct script

--- scripts/bin/remove-evicted-pods.sh
+++ scripts/bin/remove-evicted-pods.sh
@@ -2,8 +2,8 @@
 
 kubectl get nodes -o json | jq '.items[] | "kubectl taint nodes \(.metadata.name) node.kubernetes.io/disk-pressure-"' | xargs -n 1 bash -c
 
-kubectl get po --all-namespaces -o json |
+kubectl get pods --all-namespaces -o json |
     jq '.items[] | select(.status.reason!=null) | select(.status.reason | contains("Evicted")) |
-"kubectl delete po \(.metadata.name) -n \(.metadata.namespace)"' | xargs -n 1 bash -c
+"kubectl delete po \(.metadata.name) --namespace \(.metadata.namespace)"' | xargs -n 1 bash -c
 
 kubectl get nodes -o json | jq '.items[] | "kubectl taint nodes \(.metadata.name) node.kubernetes.io/disk-pressure-"' | xargs -n 1 bash -c
