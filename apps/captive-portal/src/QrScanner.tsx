f8ed6bcb066dbcb9abaa85631c8ea87f1f3d293f
Andrew Smith <andrew@andrew.codes>
2021-09-02 13:02:51 +0000

    attempt to get to work on phone

--- apps/captive-portal/src/QrScanner.tsx
+++ apps/captive-portal/src/QrScanner.tsx
@@ -13,34 +13,6 @@ type Scanner = {
   stop: () => void
 }
 
-const computeFrame = (
-  video: HTMLVideoElement,
-  canvas: HTMLCanvasElement,
-  width: number,
-  height: number
-) => {
-  const ctx = canvas.getContext("2d")
-  ctx.drawImage(video, 0, 0)
-  let region = new Path2D()
-  region.rect(0, 0, width, height)
-  ctx.clip(region)
-}
-
-const timerCallback = (
-  video: HTMLVideoElement,
-  canvas: HTMLCanvasElement,
-  width: number,
-  height: number
-) => {
-  if (video.paused || video.ended) {
-    return
-  }
-  computeFrame(video, canvas, width, height)
-  setTimeout(() => {
-    timerCallback(video, canvas, width, height)
-  }, 0)
-}
-
 const QrScannerComponent: React.FC<{
   hidden?: boolean
   onError: (error: string) => void
@@ -48,27 +20,11 @@ const QrScannerComponent: React.FC<{
   onReady: (scanner: Scanner) => void
 }> = ({ hidden = false, onError, onReady, onScan }) => {
   const videoRef = React.useRef<HTMLVideoElement>()
-  const scanAreaRef = React.useRef<HTMLCanvasElement>()
-  const width = 400
-  const height = 400
-
-  React.useEffect(() => {
-    const listener = () => {
-      timerCallback(videoRef.current, scanAreaRef.current, width, height)
-    }
-    videoRef?.current?.addEventListener("play", listener, false)
-
-    return function cleanup() {
-      videoRef.current.removeEventListener("play", listener, false)
-    }
-  }, [width, height])
-
+  const containerRef = React.useRef<HTMLDivElement>()
   let scanner
   React.useEffect(() => {
-    scanner = new QrScanner(videoRef.current, onScan, onError, () => ({
-      width,
-      height,
-    }))
+    scanner = new QrScanner(videoRef.current, onScan, onError)
+    containerRef.current.appendChild(scanner.$canvas)
     onReady(scanner)
 
     return function cleanup() {
@@ -77,9 +33,8 @@ const QrScannerComponent: React.FC<{
   }, [])
 
   return (
-    <VideoContainer hidden={hidden}>
+    <VideoContainer hidden={hidden} ref={containerRef}>
       <video style={{ display: "none" }} ref={videoRef} />
-      <canvas height={`${height}px`} ref={scanAreaRef} width={`${width}px`} />
     </VideoContainer>
   )
 }
