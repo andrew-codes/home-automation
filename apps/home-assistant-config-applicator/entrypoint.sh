448834f5d18d87c15f0dbfa77c04fa82e693dea3
Andrew Smith <andrew@andrew.codes>
2021-11-03 12:26:00 -0400

    correctly rsync

--- apps/home-assistant-config-applicator/entrypoint.sh
+++ apps/home-assistant-config-applicator/entrypoint.sh
@@ -137,13 +137,13 @@ if git-synchronize; then
     echo "Validating config"
     validate-config
     echo "Copying config"
-    rsync --recursive -I --delete . /config/appdaemon.yaml
-    rsync --recursive -I --delete . /config/configuration.yaml
-    rsync --recursive -I --delete . /config/customize.yaml
-    rsync --recursive -I --delete . /config/apps
-    rsync --recursive -I --delete . /config/dashboards
-    rsync --recursive -I --delete . /config/lovelace
-    rsync --recursive -I --delete . /config/packages
+    rsync --recursive -I --delete ./appdaemon.yaml /config/appdaemon.yaml
+    rsync --recursive -I --delete ./configuration.yaml /config/configuration.yaml
+    rsync --recursive -I --delete ./customize.yaml /config/customize.yaml
+    rsync --recursive -I --delete ./apps /config/apps
+    rsync --recursive -I --delete ./dashboards /config/dashboards
+    rsync --recursive -I --delete ./lovelace /config/lovelace
+    rsync --recursive -I --delete ./packages /config/packages
 fi
 
 ###################
