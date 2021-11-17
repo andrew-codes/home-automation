123c4e329810d31fce769a64fc1102d0f332bed8
Andrew Smith <andrew@andrew.codes>
2021-11-17 20:16:25 +0000

    captive-portal no longer requires being public

--- deployments/captive-portal/scripts/bin/deploy.sh
+++ deployments/captive-portal/scripts/bin/deploy.sh
@@ -7,4 +7,7 @@ source .external-ports.env
 set +o allexport
 popd
 
+yq eval '.spec.ports[0].nodePort=env(EXTERNAL_CAPTIVE_PORTAL_PORT)' -i service.yml
+
 kubectl apply -f captive-portal.yml
+kubectl apply -f services.yml
