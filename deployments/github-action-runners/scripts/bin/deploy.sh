99cf9a60dd7c5e9a187ee4ab2429b40b02059e6d
Andrew Smith <andrew@andrew.codes>
2021-12-22 22:37:57 +0000

    seal secrets for akv to github

--- deployments/github-action-runners/scripts/bin/deploy.sh
+++ deployments/github-action-runners/scripts/bin/deploy.sh
@@ -9,7 +9,7 @@ source .external-ports.env
 set +o allexport
 popd
 
-export GITHUB_RUNNER_TOKEN=$(az keyvault secret show --vault-name "kv-home-automation" --name "github-action-runners-GITHUB-RUNNER-TOKEN" | jq -r '.value')
+export GITHUB_RUNNER_TOKEN=$(az keyvault secret show --vault-name "kv-home-automation" --name "github-action-runners-GITHUB-TOKEN" | jq -r '.value')
 yarn seal-github-secret andrew-codes home-automation VAULT_TOKEN "$VAULT_TOKEN"
 yarn seal-github-secret andrew-codes home-automation VAULT_ADDR "http://vault:8200"
 
@@ -40,6 +40,14 @@ export MQTT_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation"
 export MQTT_USERNAME=$(az keyvault secret show --vault-name "kv-home-automation" --name "mqtt-USERNAME" | jq -r '.value')
 export GAMING_ROOM_GAMING_PC_MAC=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-GAMING-ROOM-GAMING-PC-MAC" | jq -r '.value')
 export MACHINE_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation" --name "k8s-MACHINE-PASSWORD" | jq -r '.value')
+export AZURE_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "kv-home-automation" --name "azure-subscription-id" | jq -r '.value')
+export AZURE_SERVICE_PRINCIPAL_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation" --name "azure-service-principal-password" | jq -r '.value')
+export AZURE_SERVICE_PRINCIPAL_APP_ID=$(az keyvault secret show --vault-name "kv-home-automation" --name "azure-service-principal-app-id" | jq -r '.value')
+export AZURE_SERVICE_PRINCIPAL_TENANT=$(az keyvault secret show --vault-name "kv-home-automation" --name "azure-service-principal-tenant-id" | jq -r '.value')
+yarn seal-github-secret andrew-codes home-automation AZURE_SUBSCRIPTION_ID "$AZURE_SUBSCRIPTION_ID"
+yarn seal-github-secret andrew-codes home-automation AZURE_SERVICE_PRINCIPAL_PASSWORD "$AZURE_SERVICE_PRINCIPAL_PASSWORD"
+yarn seal-github-secret andrew-codes home-automation AZURE_SERVICE_PRINCIPAL_APP_ID "$AZURE_SERVICE_PRINCIPAL_APP_ID"
+yarn seal-github-secret andrew-codes home-automation AZURE_SERVICE_PRINCIPAL_TENANT "$AZURE_SERVICE_PRINCIPAL_TENANT"
 yarn seal-github-secret andrew-codes home-automation GAMING_ROOM_GAMING_PC_MAC "$GAMING_ROOM_GAMING_PC_MAC"
 yarn seal-github-secret andrew-codes home-automation MACHINE_PASSWORD "$MACHINE_PASSWORD"
 yarn seal-github-secret andrew-codes home-automation MQTT_CONNECTION "$(
