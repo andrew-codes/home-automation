a34a1dc81e57d2820f521b647113d15c43df1596
Andrew Smith <andrew@andrew.codes>
2022-03-15 12:05:36 +0000

    minor updates

--- deployments/home-assistant/scripts/bin/ingress.sh
+++ deployments/home-assistant/scripts/bin/ingress.sh
@@ -5,8 +5,8 @@ cd ../../
 source scripts/bin/az-login.sh
 popd
 
-export HOME_ASSISTANT_DNS_USERNAME=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-DNS-USERNAME" | jq -r '.value')
-export HOME_ASSISTANT_DNS_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-DNS-PASSWORD" | jq -r '.value')
+export HOME_ASSISTANT_DNS_USERNAME=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-HOME-ASSISTANT-DNS-USERNAME" | jq -r '.value')
+export HOME_ASSISTANT_DNS_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-HOME-ASSISTANT-DNS-PASSWORD" | jq -r '.value')
 export HOME_ASSISTANT_DOMAIN=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-DOMAIN" | jq -r '.value')
 export INLETS_IP=$(az keyvault secret show --vault-name "kv-home-automation" --name "home-assistant-INLETS-IP" | jq -r '.value')
 
