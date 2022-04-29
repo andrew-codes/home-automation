151531f56cd9a2b23d06c2afd565e307b7786a38
Andrew Smith <andrew@andrew.codes>
2022-04-29 16:00:59 -0400

    publish grafana port

---
+++ deployments/grafana/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pushd .
+cd ../../
+set -o allexport
+source .external-ports.env
+set +o allexport
+popd
+
+yq eval '.spec.ports[0].nodePort=env(EXTERNAL_GRAFANA_PORT)' -i service.yml
+
+kubectl apply -f services.yml
