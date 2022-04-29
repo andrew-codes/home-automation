fb6b45c01e880abfc306122948769d9a9335eb3a
Andrew Smith <andrew@andrew.codes>
2022-04-29 22:06:04 +0000

    attempt for influxdb

--- deployments/influxdb/scripts/bin/deploy.sh
+++ deployments/influxdb/scripts/bin/deploy.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-pushd .
-cd ../../
-set -o allexport
-source .external-ports.env
-set +o allexport
-popd
-
-yq eval '.spec.ports[0].nodePort=env(EXTERNAL_INFLUXDB_PORT)' -i service.yml
-
 kubectl apply -f deployment.yml
-kubectl apply -f service.yml
