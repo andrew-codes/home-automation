04985ddfa00779469c9dda2cc3f341459baa5b3c
Andrew Smith <andrew@andrew.codes>
2022-04-29 17:55:50 -0400

    expose locally

--- deployments/influxdb/scripts/bin/deploy.sh
+++ deployments/influxdb/scripts/bin/deploy.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+pushd .
+cd ../../
+set -o allexport
+source .external-ports.env
+set +o allexport
+popd
+
+yq eval '.spec.ports[0].nodePort=env(EXTERNAL_INFLUXDB_PORT)' -i service.yml
+
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
