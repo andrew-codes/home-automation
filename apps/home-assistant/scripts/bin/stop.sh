4240cabaed28c8d07f16640df2b2500eee6b601f
Andrew Smith <andrew@andrew.codes>
2022-05-10 20:22:53 -0400

    mount

--- apps/home-assistant/scripts/bin/stop.sh
+++ apps/home-assistant/scripts/bin/stop.sh
@@ -6,5 +6,5 @@ docker kill $(docker ps -aq)
 telepresence leave home-assistant
 telepresence quit
 telepresence uninstall --all-agents
-# kubectl rollout restart deployment traffic-manager -n ambassador
+kubectl rollout restart deployment traffic-manager -n ambassador
 kubectl rollout restart deployment home-assistant
