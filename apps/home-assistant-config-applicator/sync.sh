be951fe3cd37f492d8a230ee81c4723502927a40
Andrew Smith <andrew@andrew.codes>
2022-05-12 16:27:28 -0400

    parameterize sync command

--- apps/home-assistant-config-applicator/sync.sh
+++ apps/home-assistant-config-applicator/sync.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+    echo "Must provide the absolute path to the config directory to be copied."
+    exit 1
+fi
+
 echo "Copying config"
-rsync --recursive -I --delete ./appdaemon.yaml /config/appdaemon.yaml
-rsync --recursive -I --delete ./configuration.yaml /config/configuration.yaml
-rsync --recursive -I --delete ./customize.yaml /config/customize.yaml
-rsync --recursive -I --delete ./apps /config/apps
-rsync --recursive -I --delete ./dashboards /config/dashboards
-rsync --recursive -I --delete ./lovelace /config/lovelace
-rsync --recursive -I --delete ./packages /config/packages
-rsync --recursive -I --delete ./custom_components/qrcode /config/custom_components/qrcode
+rsync --recursive -I --delete $1/appdaemon.yaml /config/appdaemon.yaml
+rsync --recursive -I --delete $1/configuration.yaml /config/configuration.yaml
+rsync --recursive -I --delete $1/customize.yaml /config/customize.yaml
+rsync --recursive -I --delete $1/apps /config/apps
+rsync --recursive -I --delete $1/dashboards /config/dashboards
+rsync --recursive -I --delete $1/lovelace /config/lovelace
+rsync --recursive -I --delete $1/packages /config/packages
+rsync --recursive -I --delete $1/custom_components/qrcode /config/custom_components/qrcode
