9359a42a6282b3ae351c850a0df70d924727175f
Andrew Smith <andrew@andrew.codes>
2022-05-12 17:52:36 -0400

    sync files properly (no double nesting of directories)

--- apps/home-assistant-config-applicator/sync.sh
+++ apps/home-assistant-config-applicator/sync.sh
@@ -9,8 +9,8 @@ echo "Copying config"
 rsync --recursive -I --delete $1/appdaemon.yaml /config/appdaemon.yaml
 rsync --recursive -I --delete $1/configuration.yaml /config/configuration.yaml
 rsync --recursive -I --delete $1/customize.yaml /config/customize.yaml
-rsync --recursive -I --delete $1/apps /config/apps
-rsync --recursive -I --delete $1/dashboards /config/dashboards
-rsync --recursive -I --delete $1/lovelace /config/lovelace
-rsync --recursive -I --delete $1/packages /config/packages
-rsync --recursive -I --delete $1/custom_components/qrcode /config/custom_components/qrcode
+rsync --recursive -I --delete $1/apps /config
+rsync --recursive -I --delete $1/dashboards /config
+rsync --recursive -I --delete $1/lovelace /config
+rsync --recursive -I --delete $1/packages /config
+rsync --recursive -I --delete $1/custom_components/qrcode /config/custom_components
