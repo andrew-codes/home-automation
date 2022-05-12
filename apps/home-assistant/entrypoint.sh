383f08458c66b7e63599a638505293f4c69cc544
Andrew Smith <andrew@andrew.codes>
2022-05-12 17:24:10 -0400

    correctly copy configs to enable prod users, locally

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -7,7 +7,7 @@ source ${ROOT}/root/generate_secrets_yaml.sh >/config/secrets.yaml
 
 if [ ! -z "$TELEPRESENCE_ROOT" ]; then
     export SHELL=/bin/bash
-    rsync --recursive /tmp /config
+    rsync --recursive /tmp/config/ /config
     /sync.sh /home-assistant-src
     chokidar "/home-assistant-src/**/*.yaml" "/home-assistant-src/custom-components/**/*.*" -c "bash -c '/sync.sh /home-assistant-src'" &
     chokidar "/config/**/*.yaml" --debounce 60000 -c "bash -c '/reload.sh | true'" &
