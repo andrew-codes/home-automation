f3c761a5faffffd95e0efd050d48715a493fc977
Andrew Smith <andrew@andrew.codes>
2022-05-15 15:20:44 -0400

    no mounts for codespaces

--- .devcontainer/save-config.sh
+++ .devcontainer/save-config.sh
@@ -12,3 +12,15 @@ cd openvpn-tmp
 if [ ! -z "${OPENVPN_CONFIG}" ]; then
     echo "${OPENVPN_CONFIG}" >vpnconfig.ovpn
 fi
+
+# Save the configuration from the secret if it is present
+if [ ! -z "${K8s_CONFIG}" ]; then
+    mkdir -p ~/.kube
+    echo -e "${K8s_CONFIG}" >~/.kube/config
+fi
+
+# Save the configuration from the secret if it is present
+if [ ! -z "${ID_RSA}" ]; then
+    mkdir -p ~/.ssh
+    echo -e "${ID_RSA}" >~/.ssh/id_rsa_2
+fi
