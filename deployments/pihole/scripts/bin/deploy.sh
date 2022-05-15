509fcfa37304d3fc4c95537ca4a8ad6f7ccecfa7
Andrew Smith <andrew@andrew.codes>
2022-05-15 12:13:37 -0400

    deploy public key to pihole

--- deployments/pihole/scripts/bin/deploy.sh
+++ deployments/pihole/scripts/bin/deploy.sh
@@ -8,11 +8,14 @@ source scripts/bin/az-login.sh
 popd
 
 export PIHOLE_PASSWORD=$(az keyvault secret show --vault-name "kv-home-automation" --name "pihole-PASSWORD" | jq -r '.value')
+export PUBLIC_KEY=$(az keyvault secret show --vault-name "kv-home-automation" --name "github-action-runners-HOME-AUTOMATION-PUBLIC-KEY" | jq -r '.value')
 
 mkdir -p .secrets
 cat >.secrets/ansible-secrets.yml <<EOL
 ---
 pihole_password: "$PIHOLE_PASSWORD"
 EOL
+
+echo -e "$PUBLIC_KEY" >.secrets/ha.pub
 
 ansible-playbook ./deploy.yml -i ./hosts.yml
