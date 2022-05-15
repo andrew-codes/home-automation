d290634897d12b2566a09893fa1ad3f5ec671ad8
Andrew Smith <andrew@andrew.codes>
2022-05-15 12:19:13 -0400

    disable deployments of z2m

--- deployments/zigbee2mqtt/scripts/bin/deploy.sh
+++ deployments/zigbee2mqtt/scripts/bin/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-pushd .
-cd ../../
-set -o allexport
-source .external-ports.env
-set +o allexport
-popd
+# pushd .
+# cd ../../
+# set -o allexport
+# source .external-ports.env
+# set +o allexport
+# popd
 
-kubectl apply -f deployment.yml
+# kubectl apply -f deployment.yml
 
-yq eval '.spec.ports[0].nodePort=env(EXTERNAL_ZIGBEE_2_MQTT_PORT)' -i service.yml
+# yq eval '.spec.ports[0].nodePort=env(EXTERNAL_ZIGBEE_2_MQTT_PORT)' -i service.yml
 
-kubectl apply -f service.yml
+# kubectl apply -f service.yml
