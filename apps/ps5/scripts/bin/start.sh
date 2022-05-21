c93266dfc4d5b1eb16b5bd7ef6f3e0abfeebaae1
Andrew Smith <andrew@andrew.codes>
2022-05-21 12:14:56 +0000

    no need for stop/start scripts
    Due to ps5 host network

--- apps/ps5/scripts/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-telepresence connect
-
-telepresence intercept "ps5" --service ps5 --env-file intercept.env --port 8080:8080 -- /bin/bash -c 'DEBUG=@ha/* nodemon src/index.ts --inspect=0.0.0.0:9233'
--- apps/ps5/scripts/bin/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Stopping"
-
-telepresence leave ps5 || true
-telepresence quit
-telepresence uninstall --all-agents
-kubectl rollout restart deployment traffic-manager -n ambassador
-kubectl rollout restart deployment ps5
