49e390816bedc12993065f3841daa6d702c0b999
Andrew Smith <andrew@andrew.codes>
2022-05-21 02:50:10 +0000

    update port

--- apps/ps5/scripts/bin/start.sh
+++ apps/ps5/scripts/bin/start.sh
@@ -2,4 +2,4 @@
 
 telepresence connect
 
-telepresence intercept "ps5" --service ps5 --env-file intercept.env --port 8080:80 -- /bin/bash -c 'DEBUG=@ha/* nodemon src/index.ts --inspect=0.0.0.0:9233'
+telepresence intercept "ps5" --service ps5 --env-file intercept.env --port 8080:8080 -- /bin/bash -c 'DEBUG=@ha/* nodemon src/index.ts --inspect=0.0.0.0:9233'
