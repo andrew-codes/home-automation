b235986a71820d10163c44be65caf25bf4aefe85
Andrew Smith <andrew@andrew.codes>
2022-05-22 20:24:35 +0000

    tests via Nx (per project)

---
+++ packages/configure-jest/src/index.js
@@ -0,0 +1,21 @@
+const { defaults } = require("jest-config")
+const { merge } = require("lodash")
+
+const defaultConfig = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.test.json",
+    },
+  },
+  coverageDirectory: "<rootDir>/.test-runs/unit",
+  collectCoverage: true,
+  preset: "ts-jest",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts"],
+  collectCoverageFrom: ["**/src/**/*.{js,ts}"],
+  coveragePathIgnorePatterns: ["/__tests__/", "/node_modules/"],
+}
+
+const configure = (overrides = {}) =>
+  merge({}, defaults, defaultConfig, overrides)
+
+module.exports = configure
