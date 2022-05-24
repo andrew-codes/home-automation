3adfcdf05172ac4be7dcbd6c00ca03992f394afc
Andrew Smith <andrew@andrew.codes>
2022-05-24 14:55:08 +0000

    pass with no tests

--- packages/configure-jest/src/index.js
+++ packages/configure-jest/src/index.js
@@ -7,6 +7,7 @@ const defaultConfig = {
       tsconfig: "<rootDir>/tsconfig.test.json",
     },
   },
+  passWithNoTests: true,
   coverageDirectory: "<rootDir>/.test-runs/unit",
   collectCoverage: true,
   preset: "ts-jest",
