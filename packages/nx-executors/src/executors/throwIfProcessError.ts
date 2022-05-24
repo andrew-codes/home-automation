5276ce7ce92fb9dab9b9466c7f1d406ce404b475
Andrew Smith <andrew@andrew.codes>
2022-05-24 14:22:04 +0000

    fix issue

--- packages/nx-executors/src/executors/throwIfProcessError.ts
+++ packages/nx-executors/src/executors/throwIfProcessError.ts
@@ -1,9 +1,8 @@
-import { identity } from "lodash"
 import { doIf } from "@ha/env-utils"
 
 const throwIfError = ({ stdout, stderr }) => {
   console.log(stdout)
-  doIf(identity(stderr))(() => {
+  doIf(() => !! stderr))(() => {
     throw new Error(stderr)
   })
 }
