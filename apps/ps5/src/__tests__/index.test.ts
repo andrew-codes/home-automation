547646598c017279565ae5949989b1961fdf6303
Andrew Smith <andrew@andrew.codes>
2022-05-25 12:52:32 +0000

    empty, failing tests for projects with no tests

---
+++ apps/ps5/src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import run from '../'
+
+describe('app', () => {
+   test('empty test for code coverage', () => {
+       throw new Error('Not implemented')
+   }); 
+})
---
+++ packages/env-utils/src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+describe('app', () => {
+   test('empty test for code coverage', () => {
+       throw new Error('Not implemented')
+   }); 
+})
