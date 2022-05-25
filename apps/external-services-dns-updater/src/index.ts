ea68803724759231e7ceec950be654e0b1383b3f
Andrew Smith <andrew@andrew.codes>
2022-05-25 14:18:53 +0000

    test coverage for external dns updater

--- apps/external-services-dns-updater/src/index.ts
+++ apps/external-services-dns-updater/src/index.ts
@@ -3,20 +3,19 @@ import { createMqtt } from "@ha/mqtt-client"
 import { google, dns_v1 } from "googleapis"
 
 const debug = createDebugger("@ha/external-services-dns-updater")
-const { SUB_DOMAINS } = process.env
 
 async function run() {
   debug("Started")
   const mqtt = await createMqtt()
   await mqtt.subscribe("homeassistant/sensor/external_ip/set")
   try {
+    const { SUB_DOMAINS } = process.env
     const subDomains = (SUB_DOMAINS ?? "").split(",")
 
     const auth = new google.auth.GoogleAuth({
       keyFile: "/.secrets/credentials.json",
       scopes: ["https://www.googleapis.com/auth/ndev.clouddns.readwrite"],
     })
-
     google.options({
       auth,
     })
@@ -38,8 +37,8 @@ async function run() {
                 ttl: 300,
                 rrdatas: [externalIP],
               },
-            })
-          )
+            }),
+          ),
         )
       } catch (error) {
         debug(error)
