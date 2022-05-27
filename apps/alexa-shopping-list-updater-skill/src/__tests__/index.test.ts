9a308fc0bfd03fc0252ded3bd21485605691549a
Andrew Smith <andrew@andrew.codes>
2022-05-27 14:14:19 +0000

    do not let test hang due to express listen

--- apps/alexa-shopping-list-updater-skill/src/__tests__/index.test.ts
+++ apps/alexa-shopping-list-updater-skill/src/__tests__/index.test.ts
@@ -1,8 +1,19 @@
+jest.mock("express")
 jest.mock("@ha/mqtt-heartbeat")
 import run from "../"
 import { createMqttHeartbeat } from "@ha/mqtt-heartbeat"
+import type { Express } from "express"
+import express from "express"
 
 describe("alexa shopping list updater skill", () => {
+  beforeEach(() => {
+    jest.mocked(express, true).mockReturnValue({
+      get: jest.fn(),
+      listen: jest.fn(),
+      post: jest.fn(),
+    } as unknown as Express)
+  })
+
   test("sets up a heartbeat health check", async () => {
     await run()
 
