d2bd5617e244333eac46a9aba1e4e1958c397f4a
Andrew Smith <andrew@andrew.codes>
2022-05-28 16:34:11 +0000

    restart deployment after deploy

--- apps/frigate/scripts/bin/deploy.sh
+++ apps/frigate/scripts/bin/deploy.sh
@@ -12,3 +12,5 @@ kubectl apply -f deployment/index.yml
 yq eval '.spec.ports[0].nodePort=env(EXTERNAL_FRIGATE_PORT)' -i deployment/service.yml
 yq eval '.spec.ports[1].nodePort=env(EXTERNAL_FRIGATE_RMTP_PORT)' -i deployment/service.yml
 kubectl apply -f deployment/service.yml
+
+kubectl rollout restart deployment frigate
