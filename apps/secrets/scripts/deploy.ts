1f155a0e866d2ece0bef8bd6912796a070d5c291
Andrew Smith <andrew@andrew.codes>
2022-06-01 20:55:00 +0000

    start of deployment of secrets

---
+++ apps/secrets/scripts/deploy.ts
@@ -0,0 +1,10 @@
+import type { ConfigurationApi } from "@ha/configuration-api"
+import type { Configuration } from "@ha/configuration-workspace"
+import { jsonnet } from "@ha/jsonnet"
+
+const run = async (
+  configurationApi: ConfigurationApi<Configuration>,
+): Promise<void> => {
+}
+
+export default run
