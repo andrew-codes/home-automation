980bf68618d9b677f3fcce8defb105406f64d236
Andrew Smith <andrew@andrew.codes>
2022-06-04 22:57:06 -0400

    openvpn app

---
+++ apps/openvpn/scripts/deploy.ts
@@ -0,0 +1,41 @@
+import type { ConfigurationApi } from "@ha/configuration-api"
+import type { Configuration } from "@ha/configuration-workspace"
+import fs from "fs/promises"
+import path from "path"
+import sh from "shelljs"
+import { throwIfError } from "@ha/shell-utils"
+
+const run = async (
+  configurationApi: ConfigurationApi<Configuration>,
+): Promise<void> => {
+  const ip = await configurationApi.get("openvpn/ip")
+  const username = await configurationApi.get("openvpn/user/1/username")
+  const password = await configurationApi.get("openvpn/user/1/password")
+
+  sh.mkdir(".secrets")
+  await fs.writeFile(
+    path.join(__dirname, "..", ".secrets", "ansible-secrets.yml"),
+    `---
+    openvpn_password: "$${password}"
+    openvpn_username: "$${username}"`,
+    "utf8",
+  )
+
+  await fs.writeFile(
+    path.join(__dirname, "..", "deployment", "hosts.yml"),
+    `
+all:
+  vars:
+    ansible_user: root
+  hosts:
+    ${ip}:`,
+    "utf8",
+  )
+  sh.env["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
+  const deployProcess = sh.exec(
+    `ansible-playbook deployment/index.yml -i deployment/hosts.yml`,
+  )
+  throwIfError(deployProcess)
+}
+
+export default run
