e55e170fd543db6f361a42135a4401f9ba53aa1f
Andrew Smith <andrew@andrew.codes>
2022-06-05 11:30:41 -0400

    re-running resets all users

--- apps/openvpn/src/setup-users.sh
+++ apps/openvpn/src/setup-users.sh
@@ -7,6 +7,27 @@ set +o allexport
 IFS=, read -a usernamesArray <<<"$usernames"
 IFS=, read -a passwordsArray <<<"$passwords"
 
+echo "Revoking all clients in order to perform reset."
+pushd
+read -a clientsToRevoke <<<"$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2)"
+cd /etc/openvpn/easy-rsa/ || return
+for i in "${!clientsToRevoke[@]}"; do
+    CLIENT="${clientsToRevoke[i]}"
+    ./easyrsa --batch revoke "$CLIENT"
+    EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+    rm -f /etc/openvpn/crl.pem
+    cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
+    chmod 644 /etc/openvpn/crl.pem
+    find /home/ -maxdepth 2 -name "$CLIENT.ovpn" -delete
+    rm -f "/root/$CLIENT.ovpn"
+    sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
+    cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
+    echo ""
+    echo "Certificate for client $CLIENT revoked."
+done
+popd
+
+echo "Creating all clients."
 export MENU_OPTION="1"
 export PASS="2"
 for i in "${!usernamesArray[@]}"; do
