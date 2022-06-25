0681634a29d477ea97b5ea3fb59f318b698ccbfa
Andrew Smith <andrew@andrew.codes>
2022-06-25 15:56:00 +0000

    fix test

--- apps/ps5/src/__tests__/index.test.ts
+++ apps/ps5/src/__tests__/index.test.ts
@@ -2,12 +2,11 @@ jest.mock("@ha/mqtt-client")
 jest.mock("@ha/http-heartbeat")
 import run from "../"
 import { createHeartbeat } from "@ha/http-heartbeat"
-import { healthUrlPath } from "../../scripts/config"
 
 describe("ps5", () => {
   test("sets up a heartbeat health check", async () => {
     await run()
 
-    expect(createHeartbeat).toBeCalledWith(healthUrlPath)
+    expect(createHeartbeat).toBeCalledWith("/health")
   })
 })
