ceff1e1eee420e384fed3c9168b402ef92e7ff60
Andrew Smith <andrew@andrew.codes>
2022-07-09 14:41:38 +0000

    enable manual deployments

---
+++ apps/manual-deployment/scripts/deploy.ts
@@ -0,0 +1,35 @@
+import type { ConfigurationApi } from "@ha/configuration-api"
+import type { Configuration } from "@ha/configuration-workspace"
+import { isEmpty } from "lodash"
+import { Octokit } from "@octokit/core"
+
+const run = async (
+  configurationApi: ConfigurationApi<Configuration>,
+): Promise<void> => {
+  const projects = JSON.parse(process.argv[2])
+    .overrides["target-projects"].split(",")
+    .filter((project) => !isEmpty(project))
+  if (isEmpty(projects)) {
+    throw new Error(
+      "No target projects specified. Did you forget `--target-projects=p1,p2`. ",
+    )
+  }
+
+  console.log(`Deploying proejcts: ${projects.join(",")}`)
+  const token = await configurationApi.get("github/token")
+  const owner = await configurationApi.get("repository/owner")
+  const repo = await configurationApi.get("repository/name")
+  const octokit = new Octokit({
+    auth: token,
+  })
+  await octokit.request(`POST /repos/${owner}/${repo}/dispatches`, {
+    owner,
+    repo,
+    event_type: "manual-deploy",
+    client_payload: {
+      projects: projects.join(","),
+    },
+  })
+}
+
+export default run
