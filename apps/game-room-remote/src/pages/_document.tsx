340a84476efe042740f2bfb50a37d13c9c551633
Andrew Smith <andrew@andrew.codes>
2022-07-19 17:05:46 +0000

    some basic layout ideas

--- apps/game-room-remote/src/pages/_document.tsx
+++ apps/game-room-remote/src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import Document, { Head, Main, NextScript } from "next/document"
-// Import styled components ServerStyleSheet
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document<{ styleTags: any }> {
@@ -19,11 +18,7 @@ export default class MyDocument extends Document<{ styleTags: any }> {
   render() {
     return (
       <html>
-        <Head>
-          <title>My page</title>
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
-        </Head>
+        <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
           <NextScript />
--- apps/game-room-remote/src/pages/index.tsx
+++ apps/game-room-remote/src/pages/index.tsx
@@ -1,7 +1,69 @@
 import * as React from "react"
+import styled, { createGlobalStyle } from "styled-components"
+import {
+  Content,
+  LeftSidebarWithoutResize,
+  Main,
+  PageLayout,
+  useLeftSidebarFlyoutLock,
+} from "@atlaskit/page-layout"
+import {
+  ButtonItem,
+  Header,
+  NavigationContent,
+  NavigationHeader,
+  NestableNavigationContent,
+  NestingItem,
+  Section,
+  SideNavigation,
+} from "@atlaskit/side-navigation"
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: Open-Sans, Helvetica, Sans-Serif;
+  margin: 0;
+  padding: 0;
+  background: rgb(39,40,38);
+  --ds-surface: rgb(39,40,38);
+  color: rgb(204,207,194);
+  --ds-text: rgb(204,207,194);
+  --ds-text-subtlest: rgb(204,207,194);
+}
+#__next {
+  height: 100vh;
+}
+`
+const NavBorder = styled.div`
+  border-right: 1px solid rgb(88, 89, 86);
+`
 
 function Index() {
-  return <div>Hello world</div>
+  return (
+    <>
+      <GlobalStyle />
+      <PageLayout>
+        <Content>
+          <NavBorder>
+            <LeftSidebarWithoutResize width={350}>
+              <SideNavigation label="Game Room Navigation">
+                <NavigationHeader>
+                  <Header>Game Room</Header>
+                </NavigationHeader>
+                <NavigationContent>
+                  <Section title="Find">
+                    <ButtonItem>Search</ButtonItem>
+                  </Section>
+                </NavigationContent>
+              </SideNavigation>
+            </LeftSidebarWithoutResize>
+          </NavBorder>
+          <Main>
+            <div>Hello</div>
+          </Main>
+        </Content>
+      </PageLayout>
+    </>
+  )
 }
 
 export default Index
