d87093bb29fae51b42dbfe7c2049a396642492bc
Andrew Smith <andrew@andrew.codes>
2022-08-06 15:14:44 +0000

    logging for pihole update

--- apps/pihole/scripts/update.ts
+++ apps/pihole/scripts/update.ts
@@ -24,9 +24,7 @@ const run = async (
 
   sh.env["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
   throwIfError(
-    sh.exec(`ansible-playbook deployment/update.yml -i .secrets/hosts.yml;`, {
-      silent: true,
-    }),
+    sh.exec(`ansible-playbook deployment/update.yml -i .secrets/hosts.yml;`),
   )
 }
 
