b77074b71e2a2f882fd11d3df5bf40661d428bf6
Andrew Smith <andrew@andrew.codes>
2022-08-07 20:32:37 +0000

    fixes?

--- apps/elk-stack/scripts/deploy.ts
+++ apps/elk-stack/scripts/deploy.ts
@@ -4,14 +4,13 @@ import type { ConfigurationApi } from "@ha/configuration-api"
 import type { Configuration } from "@ha/configuration-workspace"
 import { jsonnet } from "@ha/jsonnet"
 import { kubectl } from "@ha/kubectl"
-import { throwIfError } from "@ha/shell-utils"
 import { name } from "./config"
 
 const run = async (
   configurationApi: ConfigurationApi<Configuration>,
 ): Promise<void> => {
-  throwIfError(sh.exec(`kubectl create -f https://download.elastic.co/downloads/eck/2.3.0/crds.yaml;`))
-  throwIfError(sh.exec(`kubectl apply -f https://download.elastic.co/downloads/eck/2.3.0/operator.yaml;`))
+  sh.exec(`kubectl create -f https://download.elastic.co/downloads/eck/2.3.0/crds.yaml;`)
+  sh.exec(`kubectl apply -f https://download.elastic.co/downloads/eck/2.3.0/operator.yaml;`)
 
   const elasticPort = await configurationApi.get("elk-stack/elastic-search/port/external")
   const kibanaPort = await configurationApi.get("elk-stack/kibana/port/external")
