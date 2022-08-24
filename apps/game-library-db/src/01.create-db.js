217054081d5f7dce35c78088f69e546de2f8cf90
Andrew Smith <andrew@andrew.codes>
2022-08-24 02:39:22 +0000

    removal of unncessary script

--- apps/game-library-db/src/01.create-db.js
+++
@@ -1,5 +0,0 @@
-db = new Mongo().getDB("gameLibrary")
-
-db.createCollection('games', { capped: false })
-db.createCollection('platforms', { capped: false })
-db.createCollection('files', { capped: false })
