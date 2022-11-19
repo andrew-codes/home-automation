36f269a0039f3fdddff2e2d490e1eb3a78b3122b
Andrew Smith <andrew@andrew.codes>
2022-11-19 13:08:52 -0500

    error handling

--- apps/guest-registrar/src/redux/sagas/updatePorters.ts
+++ apps/guest-registrar/src/redux/sagas/updatePorters.ts
@@ -5,28 +5,32 @@ import type { UpdatePortersAction } from "../types"
 const logger = createLogger()
 
 function* updatePorters(action: UpdatePortersAction) {
-  logger.info("Updating Porters", action.payload)
-  const response: Response = yield call(
-    fetch,
-    `https://portersetup.com/api/locations/${process.env.WIFI_PORTER_LOCATION_ID}`,
-    {
-      method: "PATCH",
-      headers: {
-        authorization: `Bearer ${process.env.WIFI_PORTER_API_KEY}`,
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        plaintext_credential: {
-          ssid: action.payload.name,
-          key: action.payload.passPhrase,
+  try {
+    logger.info("Updating Porters", action.payload)
+    const response: Response = yield call(
+      fetch,
+      `https://portersetup.com/api/locations/${process.env.WIFI_PORTER_LOCATION_ID}`,
+      {
+        method: "PATCH",
+        headers: {
+          authorization: `Bearer ${process.env.WIFI_PORTER_API_KEY}`,
+          "content-type": "application/json",
         },
-        account_password: process.env.WIFI_PORTER_ACCOUNT_PASSWORD,
-      }),
-    },
-  )
-  logger.info(`Porter update response: ${response.ok} ${response.statusText}`)
-  const responseBody = yield call(response.json)
-  logger.debug(JSON.stringify(responseBody))
+        body: JSON.stringify({
+          plaintext_credential: {
+            ssid: action.payload.name,
+            key: action.payload.passPhrase,
+          },
+          account_password: process.env.WIFI_PORTER_ACCOUNT_PASSWORD,
+        }),
+      },
+    )
+    logger.info(`Porter update response: ${response.ok} ${response.statusText}`)
+    const responseBody = yield call(response.json)
+    logger.debug(JSON.stringify(responseBody))
+  } catch (error) {
+    logger.error(error)
+  }
 }
 
 export default updatePorters
