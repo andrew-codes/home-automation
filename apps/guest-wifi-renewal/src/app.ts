22845b8cf3612e7f92becd9ce7b55cbc731d5501
Andrew Smith <andrew@andrew.codes>
2022-11-22 09:31:53 -0500

    guest wifi renewal hearbeat

--- apps/guest-wifi-renewal/src/app.ts
+++ apps/guest-wifi-renewal/src/app.ts
@@ -1,12 +1,12 @@
 import { createLogger } from "@ha/logger"
 import { createMqtt } from "@ha/mqtt-client"
-import { createHeartbeat } from "@ha/mqtt-heartbeat"
+import { createHeartbeat } from "@ha/http-heartbeat"
 import { createUnifi } from "@ha/unifi-client"
 
 const logger = createLogger()
 
 const run = async (): Promise<void> => {
-  await createHeartbeat("guest-wifi-renewal-service")
+  await createHeartbeat()
 
   const mqtt = await createMqtt()
   mqtt.subscribe("homeassistant/group/guest/renew")
@@ -15,11 +15,11 @@ const run = async (): Promise<void> => {
 
   mqtt.on("message", async (topic, payload) => {
     try {
-      logger.info('Mmessage received', topic, payload)
+      logger.info("Message received", topic, payload)
       if (topic !== "homeassistant/group/guest/renew") return
       const macAddresses = payload.toString().replace(/ /g, "").split(",")
       for (const mac of macAddresses) {
-        logger.info('Authorizing MAC', mac)
+        logger.info("Authorizing MAC", mac)
         await unifi.authorizeGuest(mac, 4320)
       }
     } catch (error) {
