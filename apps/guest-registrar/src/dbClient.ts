1660c45b6544a64b9e1b2c99724a8022da8b3386
Andrew Smith <andrew@andrew.codes>
2022-12-09 11:11:07 -0500

    correct env vars

--- apps/guest-registrar/src/dbClient.ts
+++ apps/guest-registrar/src/dbClient.ts
@@ -7,10 +7,10 @@ const logger = createLogger()
 const getClient = async () => {
   logger.info("Getting DB client")
   if (!client) {
-    const username = process.env.DB_USERNAME
-    const password = process.env.DB_PASSWORD
-    const host = process.env.DB_HOST
-    const port = process.env.DB_PORT
+    const username = process.env.GUEST_DB_USERNAME
+    const password = process.env.GUEST_DB_PASSWORD
+    const host = process.env.GUEST_DB_HOST
+    const port = process.env.GUEST_DB_PORT
     const connectionUrl = `mongodb://${username}:${password}@${host}:${port}`
     client = new MongoClient(connectionUrl)
     logger.info("Connecting a new client")
