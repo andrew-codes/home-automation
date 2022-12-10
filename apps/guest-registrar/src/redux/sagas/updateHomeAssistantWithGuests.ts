6da350bd404416912e45275a9dfe8288127b7dc9
Andrew Smith <andrew@andrew.codes>
2022-12-10 15:06:23 -0500

    publish correct message to add guests

--- apps/guest-registrar/src/redux/sagas/updateHomeAssistantWithGuests.ts
+++ apps/guest-registrar/src/redux/sagas/updateHomeAssistantWithGuests.ts
@@ -1,7 +1,7 @@
 import { AsyncMqttClient, IPublishPacket } from "@ha/mqtt-client"
 import { createLogger } from "@ha/logger"
 import { call, put } from "redux-saga/effects"
-import { MongoClient, Db, WithId, Document, FindCursor } from "mongodb"
+import { MongoClient, WithId, Document } from "mongodb"
 import { createMqtt } from "@ha/mqtt-client"
 import getMongoDbClient from "../../dbClient"
 import { AddGuestAction } from "../types"
@@ -22,15 +22,16 @@ function* updateHomeAssistantWithGuests(action: AddGuestAction) {
     const macs = results.map((r) => r.mac)
     logger.info("Registered guest MACs")
     logger.debug(JSON.stringify(macs, null, 2))
+    const mqtt: AsyncMqttClient = yield call(createMqtt)
     for (const mac in macs) {
-      const mqtt: AsyncMqttClient = yield call(createMqtt)
+      logger.info(`Registering ${mac} with HA`)
       yield call<
         (
           topic: string,
           message: string | Buffer,
           { qos }: { qos: number },
         ) => Promise<IPublishPacket>
-      >(mqtt.publish.bind(mqtt), `"homeassistant/group/guests/add"`, mac, {
+      >(mqtt.publish.bind(mqtt), "homeassistant/group/guests/add", mac, {
         qos: 1,
       })
     }
