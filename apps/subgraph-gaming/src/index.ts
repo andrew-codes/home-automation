f622074085c4fae53e28489a83466b942f595cb0
Andrew Smith <andrew@andrew.codes>
2022-12-16 13:30:16 -0500

    test schema

--- apps/subgraph-gaming/src/index.ts
+++ apps/subgraph-gaming/src/index.ts
@@ -10,9 +10,10 @@ import gql from "graphql-tag"
 import { buildSubgraphSchema } from "@apollo/subgraph"
 import { createLogger } from "@ha/logger"
 import { createHeartbeat } from "@ha/http-heartbeat"
-import { Db, MongoClient } from "mongodb"
+import { Db, MongoClient, ObjectId } from "mongodb"
 import { first } from "lodash"
 import { GraphQLError } from "graphql"
+import { GraphQLResolverMap } from "@apollo/subgraph/dist/schema-helper"
 
 const logger = createLogger()
 type GraphContext = {
@@ -24,24 +25,102 @@ const typeDefs = gql`
   extend schema @link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@shareable"])
 
   type Query {
-    me: User
+    games: [Game!]!
+    genres: [GameGenre!]!
   }
 
   type User @key(fields: "id") {
     id: ID!
     username: String
   }
+
+  type GamePlatform @key(fields: "id") {
+    id ID!
+    name String!
+  }
+
+  type GameGenres @key(fields: "id") {
+    id ID!
+    name String!
+  }
+
+  type GameSeries @key(fields: "id") {
+    id ID!
+    name String!
+  }
+
+  type GameSource @key(fields: "id") {
+    id ID!
+    name String!
+  }
+
+  type Game @key(fields: "id") {
+    id ID!
+    communityScore Int
+    criticScore Int
+    description String
+    gameId String!
+    genres [GameGenre!]!
+    isInstalled Boolean!
+    isInstalling Boolean!
+    isLaunching Boolean!
+    isRunning Boolean!
+    isUninstalling Boolean!
+    name String!
+    platforms [GamePlatforms!]!
+    releaseYear Int
+    series [GameSeries!]!
+    source GameSource!
+  }
 `
 
-const resolvers = {
+const resolvers: GraphQLResolverMap<GraphContext> = {
   Query: {
-    me() {
-      return { id: "1", username: "@ava" }
+    genres(parent, args, ctx) {
+      return ctx.db.collection("genres").find({})
+    },
+    games(parent, args, ctx) {
+      return ctx.db.collection("games").find({})
     },
   },
-  User: {
-    __resolveReference(user, ctx) {
-      return user
+  GameGenre: {
+    __resolveReference(ref, ctx: GraphContext) {
+      return ctx.db.collection("genres").find({ _id: new ObjectId(ref.id) })
+    },
+    genres(parent, args, ctx) {
+      return ctx.db
+        .collection("genres")
+        .find({ _id: { $in: parent.genreIds.map((id) => new ObjectId(id)) } })
+    },
+  },
+  GamePlatform: {
+    __resolveReference(ref, ctx: GraphContext) {
+      return ctx.db.collection("platforms").find({ _id: new ObjectId(ref.id) })
+    },
+    genres(parent, args, ctx) {
+      return ctx.db.collection("platforms").find({
+        _id: { $in: parent.platformIds.map((id) => new ObjectId(id)) },
+      })
+    },
+  },
+  GameSeries: {
+    __resolveReference(ref, ctx: GraphContext) {
+      return ctx.db.collection("series").find({ _id: new ObjectId(ref.id) })
+    },
+    genres(parent, args, ctx) {
+      return ctx.db
+        .collection("series")
+        .find({ _id: { $in: parent.seriesIds.map((id) => new ObjectId(id)) } })
+    },
+  },
+  GameSource: {
+    __resolveReference(ref, ctx: GraphContext) {
+      return ctx.db.collection("sources").find({ _id: new ObjectId(ref.id) })
+    },
+    genres(parent, args, ctx) {
+      return ctx.db
+        .collection("platforms")
+        .find({ _id: new ObjectId(parent.sourceId) })
     },
   },
 }
@@ -51,7 +130,10 @@ const run = async () => {
   const httpServer = await createHeartbeat("/health", http.createServer(app))
 
   const server = new ApolloServer<GraphContext>({
-    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+    schema: buildSubgraphSchema({
+      typeDefs,
+      resolvers: resolvers as GraphQLResolverMap<any>,
+    }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     formatError: (formattedError, error) => {
       logger.error(formattedError.message, formattedError)
