23a9a11dc553ac3defb36f0ec95653c8c6c7719e
Andrew Smith <andrew@andrew.codes>
2022-12-16 14:21:32 -0500

    correct build error

--- packages/github-secrets/scripts/compile.ts
+++ packages/github-secrets/scripts/compile.ts
@@ -1,11 +1,12 @@
 import build from "@ha/build-ts"
+import path from "path"
 import pkg from "../package.json"
 
 const run = async (): Promise<void> => {
   await build({
-    entryPoints: ["src/bin/seal.ts"],
+    entryPoints: [path.join(__dirname, "..", "src", "bin", "seal.ts")],
     external: Object.keys(pkg.dependencies),
-    outfile: "dist/seal.js",
+    outfile: path.join(__dirname, "dist", "seal.js"),
   })
 }
 
