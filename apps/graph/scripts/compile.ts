efc58f048564e91c97836920e61e0386841d2a84
Andrew Smith <andrew@andrew.codes>
2022-12-19 14:37:43 -0500

    must await

--- apps/graph/scripts/compile.ts
+++ apps/graph/scripts/compile.ts
@@ -4,10 +4,9 @@ import sh from "shelljs"
 const run = async (): Promise<void> => {
   sh.mkdir("-p", "dist")
   sh.env["APOLLO_ELV2_LICENSE"] = "yes"
-  throwIfError(
+  await throwIfError(
     sh.exec(
       `rover supergraph compose --config src/supergraph.yaml > dist/schema.graphql`,
-      { async: true },
     ),
   )
 }
