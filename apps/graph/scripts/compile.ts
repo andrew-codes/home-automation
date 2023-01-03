7b6d2a012f4e88685d64240fad98c62030264c3f
Andrew Smith <andrew@andrew.codes>
2023-01-03 11:06:07 -0500

    do not rely on deployed service to compile graph

--- apps/graph/scripts/compile.ts
+++ apps/graph/scripts/compile.ts
@@ -1,8 +1,16 @@
 import { throwIfError } from "@ha/shell-utils"
+import path from "path"
 import sh from "shelljs"
 
 const run = async (): Promise<void> => {
-  sh.mkdir("-p", "dist")
+  const buildDir = path.join(__dirname, "..", "dist", "build")
+  sh.mkdir("-p", buildDir)
+  sh.cp(
+    "-R",
+    path.join(__dirname, "..", "..", "**", "generated", "*.graphql"),
+    buildDir,
+  )
+
   sh.env["APOLLO_ELV2_LICENSE"] = "accept"
   await throwIfError(
     sh.exec(
