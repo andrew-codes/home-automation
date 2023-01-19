c67449aad14002c0b1699e28205c6865fcbe9f08
Andrew Smith <andrew@andrew.codes>
2023-01-19 08:34:04 -0500

    enable large request bodies for docker registry pushes

--- apps/proxy/scripts/compile.ts
+++ apps/proxy/scripts/compile.ts
@@ -42,6 +42,7 @@ server {
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_http_version 1.1;
     proxy_set_header Connection "upgrade";
+    client_max_body_size 0;
   }
 `,
   ).join(`
