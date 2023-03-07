df2ab368f02917c648f56e263c441e66bd08b1b4
Andrew Smith <andrew@andrew.codes>
2023-03-07 07:47:21 -0500

    use game area

--- apps/game-library-updater/src/handlers/index.ts
+++ apps/game-library-updater/src/handlers/index.ts
@@ -3,12 +3,14 @@ import gameAttributes from "./gameAttributes"
 import gameAssets from "./gameAssets"
 import storePlayniteGames from "./storePlayniteGames"
 import gameState from "./gameState"
+import gameArea from "./gameArea"
 
 const handlers: MessageHandler[] = [
   storePlayniteGames,
   gameAttributes,
   gameAssets,
   gameState,
+  gameArea,
 ]
 
 export default handlers
