3ff2bb32478b92a93ea6aac603613f339bbaead0
Andrew Smith <andrew@andrew.codes>
2023-06-04 17:07:14 -0400

    treat lovelace dashboards like UI
    - not stored in src control
    - edited via sshfs

--- apps/home-assistant-config-applicator/sync.sh
+++ apps/home-assistant-config-applicator/sync.sh
@@ -9,8 +9,8 @@ echo "Copying config"
 rsync --recursive -I --delete $1/appdaemon.yaml /config/appdaemon.yaml
 rsync --recursive -I --delete $1/configuration.yaml /config/configuration.yaml
 rsync --recursive -I --delete $1/customize.yaml /config/customize.yaml
-rsync --recursive -I --delete $1/ui-lovelace.yaml /config/ui-lovelace.yaml
+# rsync --recursive -I --delete $1/ui-lovelace.yaml /config/ui-lovelace.yaml
 rsync --recursive -I --delete $1/apps/ /config/apps
 rsync --recursive -I --delete $1/dashboards/ /config/dashboards
-rsync --recursive -I --delete $1/lovelace/ /config/lovelace
+# rsync --recursive -I --delete $1/lovelace/ /config/lovelace
 rsync --recursive -I --delete $1/packages/ /config/packages
