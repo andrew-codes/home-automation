a1300c216d545da85fe2847788d65673c0e619ab
Andrew Smith <andrew@andrew.codes>
2024-05-20 20:21:05 +0000

    fix: when things  happen

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -15,6 +15,3 @@ ONEPASSWORD_TOKEN="$OP_CONNECT_TOKEN"
 ONEPASSWORD_VAULT_ID="$OP_CONNECT_VAULT_ID"
 
 " >.secrets.env
-
-corepack enable
-corepack prepare --activate yarn@4.1.1
--- .devcontainer/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ -f .secrets.env ]; then
-    set -o allexport
-    source .secrets.env
-    set +o allexport
-fi
