121b19ccb5d7fd48c50c568d5effbd4f18b15e56
Andrew Smith <andrew@andrew.codes>
2024-05-20 09:48:10 -0400

    support pulling secrets from op and env vars

--- .devcontainer/save-config.sh
+++ .devcontainer/save-config.sh
@@ -1,51 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Switch to the .devcontainer folder
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-if [ -f devcontainer.env ]; then
-    set -o allexport
-    source devcontainer.env
-    set +o allexport
-fi
-
-# Create a temporary directory
-mkdir -p .secrets
-cd .secrets
+export ONEPASSWORD_VAULT_ID="$OP_CONNECT_VAULT_ID"
 
 # Save the configuration from the secret if it is present
-if [ ! -z "${OPENVPN_CONFIG}" ]; then
-    echo "${OPENVPN_CONFIG}" >.secrets/vpnconfig.ovpn
-fi
+mkdir -p ~/.kube
+echo -e "$(op read op://$OP_VAULT_ID/s6mbwk4ppivpoyjgmzba3nsyu4/secret-value)}" >~/.kube/config
 
-# Save the configuration from the secret if it is present
-if [ ! -z "${K8S_CONFIG}" ]; then
-    mkdir -p ~/.kube
-    echo -e "${K8S_CONFIG}" >~/.kube/config
-fi
-
-# Save the configuration from the secret if it is present
-if [ ! -z "${ID_RSA}" ]; then
-    mkdir -p ~/.ssh
-    echo -e "${ID_RSA}" >~/.ssh/id_rsa
-    chmod 600 ~/.ssh/id_rsa
-fi
-
-if [ ! -z "${PROXMOX_ID_RSA}" ]; then
-    mkdir -p ~/.ssh
-    echo -e "${PROXMOX_ID_RSA}" >~/.ssh/proxmox
-    chmod 600 ~/.ssh/proxmox
-fi
-
-# Save the configuration from the secret if it is present
-if [ ! -z "${VPN_CREDS}" ]; then
-    echo -e "${VPN_CREDS}" >.secrets/vpn.creds
-fi
-
-# Save the configuration from the secret if it is present
-if [ ! -z "${HOME_AUTOMATION_SECRETS_ENV}" ]; then
-    echo -e "${HOME_AUTOMATION_SECRETS_ENV}" >../../.secrets.env
-fi
-
-echo "{}" > ~/.docker/config.json
+mkdir -p ~/.docker
+echo "{}" >~/.docker/config.json
--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -4,14 +4,12 @@ set -e
 mkdir -p ~/.docker
 echo "{}" >~/.docker/config.json
 
-if [ -f .secrets.env ]; then
-    set -o allexport
-    source .secrets.env
-    set +o allexport
+export ONEPASSWORD_SERVER_URL="$OP_CONNECT_HOST"
+export ONEPASSWORD_TOKEN="$OP_CONNECT_TOKEN"
+export ONEPASSWORD_VAULT_ID="$OP_CONNECT_VAULT_ID"
 
-    yarn
-    yarn nx run secrets:dev-env
-fi
+yarn
+yarn nx run secrets:dev-env
 
 ./.devcontainer/save-config.sh
 # ./start-telepresence.sh
