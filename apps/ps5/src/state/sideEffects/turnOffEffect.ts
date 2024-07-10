d15a42c0b459026e0ea77635853c2e9a7cffda94
Andrew Smith <andrew@andrew.codes>
2024-07-10 08:04:23 -0400

    retry up to 5 times to turn off PS

--- apps/ps5/src/state/sideEffects/turnOffEffect.ts
+++ apps/ps5/src/state/sideEffects/turnOffEffect.ts
@@ -1,4 +1,5 @@
 import { createLogger } from "@ha/logger"
+import { delay } from "redux-saga/effects"
 import sh from "shelljs"
 import { PlayStation } from "../device.slice"
 
@@ -6,13 +7,21 @@ const logger = createLogger()
 
 function* turnOffEffect(ps: PlayStation) {
   logger.info(`Turning off PlayStation (${ps.id})`)
-  const { stdout, stderr, code } = sh.exec(
-    `playactor standby --ip ${ps.address.ip} --timeout 5000 --connect-timeout 5000 --no-open-urls --no-auth;`,
-    { timeout: 5000 },
-  )
-  logger.info(stdout.toString())
-  if (code !== 0) {
-    logger.error(stderr.toString(), code)
+  let shouldContinue = true
+  const maxAttempts = 5
+  let attempts = 0
+  while (shouldContinue) {
+    const { stdout, stderr, code } = sh.exec(
+      `playactor standby --ip ${ps.address.ip} --timeout 5000 --connect-timeout 5000 --no-open-urls --no-auth;`,
+      { timeout: 5000 },
+    )
+    logger.info(stdout.toString())
+    if (code !== 0) {
+      logger.error(stderr.toString(), code)
+    }
+    attempts++
+    shouldContinue = code !== 0 && attempts < maxAttempts
+    yield delay(1500)
   }
 }
 
