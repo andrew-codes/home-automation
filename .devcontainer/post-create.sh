29c8b1228331892d0e5947cd2b99a168b7fa92d7
Andrew Smith <andrew@andrew.codes>
2024-10-24 19:06:36 +0000

    do not create .secrets.env template

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,13 +7,6 @@ corepack enable
 corepack use yarn
 yarn set version 4.1.1
 
-touch .secrets.env
-echo -e "
-ONEPASSWORD_SERVER_URL=""
-ONEPASSWORD_TOKEN=""
-ONEPASSWORD_VAULT_ID=""
-" >.secrets.env
-
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 go install -a github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb@latest
 ln -s $(go env GOPATH)/bin/jsonnet /usr/local/bin
